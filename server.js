a939c334f6a39c66f5cc505c0ef9fb42bdedc2ba
fireteam99 <raymancheungsy@gmail.com>
2018-09-12 14:58:59 -0400

    redid entire file system and refractored static html pages to ejs

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
-const app = require('express');
+const app = express();
 
 // EJS setup
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,13 +11,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middleware
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(cors());
 
 // view routes
-app.route('/', (req, res) => {
-  res.render('home/index');
-}
+app.get('/', (req, res) => {
+  res.render('pages/Home');
+});
+
+app.get('/buy', (req, res) => {
+  res.render('pages/Buy-Exams');
+});
+
+app.get('/sell', (req, res) => {
+  res.render('pages/Post-Exam-Form');
+});
+
+app.get('/exam-details', (req, res) => {
+  res.render('pages/Exam-Details');
+});
+
+app.get('/exams', (req, res) => {
+  res.render('pages/Exam-Page-Template');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
