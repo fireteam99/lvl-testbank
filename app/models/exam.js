542d196e00251bbb0e467b4643bf59365a193826
fireteam99 <raymancheungsy@gmail.com>
2019-01-04 12:31:41 -0500

    test schema

--- app/models/exam.js
+++ app/models/exam.js
@@ -1,1 +1,36 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const CommentSchema = require('./comment.js')
+
+const ExamSchema = new mongoose.Schema({
+    buyerCount: {
+        type: Number,
+        min: [0, 'Buyer count must be positive.']
+    },
+    seller: {
+        type: String,
+        required: [true, 'Seller is required.'],
+        index: true
+    },
+    date: {
+        type: Date,
+        required: [true, 'Date posted is required.']
+    },
+    netRating: {
+        type: Number
+    },
+    likes: {
+        type: Number
+    },
+    dislikes: {
+        type: Number
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: [0, 'Price must be positive']
+    },
+    comments: {
+        type: [CommentSchema]
+    }
+});
